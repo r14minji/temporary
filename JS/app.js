29214e6aab426e952d57f467fe37455888d2f607
DESKTOP-U766TUC\user <rkdalswl0728@gmail.com>
2021-05-25 16:14:38 +0900

    review1

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,56 @@
-let number
+import {
+  $GridLayout,
+  $WriteForm,
+  $TodoList,
+  $UsingList,
+  $DoneList,
+  card,
+  $ModifyModal,
+} from "./component.js"
+
+import {createDate} from "./utils/utils.js"
+
+//3. 데이터는 배열이다. parse는 객체로 변환시켜줌.
+const todos = [] || JSON.parse(localStorage.getItem("todos"))
+
+//4.입력한 데이터가 리스트로 보이려면 cardrender을 실행시켜야함.
+
+//2. 입력하면 저장된다:함수만들기
+const FormEvnet = () => {
+  //html에서 가지고 온 값을 담는 변수명은 $붙이기
+  const $form = document.querySelector('.form_area')
+  const $todoInput = document.querySelector('.title_input');
+  const $contentInput = document.querySelector('.content_input');
+  
+  $form.addEventListener("submit", (e) => {
+    e.preventdefault;
+    const title = $todoInput.value;
+    const contents = $contentInput.value;
+    const formdata = {title, contents, date: createDate()}
+    if (!title) return alert("할 일을 입력해주세요.");
+    if (!contents) return alert("내용을 입력해주세요.");
+    //저장장소는 localstorage
+    localStorage.setItem("todos",JSON.stringify(todos));
+    todos.push(formdata)
+
+    //rneder을 실행시켜야 localstorage에 저장된다!
+    render();
+  })
+
+}
+
+//1. 함수를 실행시켜줄 때 마다 화면을 그리도록
+const render = () => {
+  const rootArea = document.querySelector('#root');
+  rootArea.innerHTML = $GridLayout;
+  const col = document.querySelectorAll('.column');
+  col[0].innerHTML = $WriteForm
+  col[1].innerHTML = $TodoList;
+  col[2].innerHTML = $UsingList;
+  col[3].innerHTML = $DoneList;
+
+FormEvnet();
+
+}
+
+render();
---
+++ JS/component.js
@@ -0,0 +1,118 @@
+export const $GridLayout = `
+<div class="ui grid">
+  <div class="four wide column"></div>
+  <div class="four wide column"></div>
+  <div class="four wide column"></div>
+  <div class="four wide column"></div>
+</div>
+`;
+
+export const $WriteForm = `
+<div class="form_area">
+  <form class="ui form">
+    <div class="field">
+      <label>TITLE</label>
+      <input class="title_input" type="text" name="first-name" placeholder="title">
+    </div>
+    <div class="field">
+      <label>Content</label>
+      <input class="content_input" type="text" name="last-name" placeholder="content">
+    </div>
+    <button class="ui primary button" type="submit">Submit</button>
+  </form>
+</div>
+`
+
+export const card = (item, btnName) => {
+  return `
+  <div class="ui cards" style="width: 95%">
+  <div class="card" style="width: 100%">
+      <div class="btn_area">
+          ${
+            btnName !== "todos"
+              ? `
+              <button class="${btnName} prev"><i class="large red history icon"></i></button>
+              `
+              : ""
+          }
+          <button class="${btnName} next">
+            <i class="large teal check circle outline icon link"></i>
+          </button>
+          <button class="${btnName} modify">
+            <i class="large edit outline icon link"></i>
+          </button>
+          <button class="${btnName} delete">
+            <i class="large grey red close icon link"></i>
+          </button>
+      </div>
+      <div class="content">
+          <div class="header">${item.title}</div>
+          <div class="meta">${item.date}</div>
+          <div class="description">${item.contents}</div>
+      </div>
+  </div>
+</div>
+  `
+};
+
+export const $TodoList = `
+<div class="kanban_board todo">
+  <h2 class="ui header">
+    <i class="keyboard icon"></i>
+    <div class="content">
+      TODO
+    </div>
+  </h2>
+  <div class="list_area">
+    
+  </div>
+</div>
+`
+
+export const $UsingList = `
+<div class="kanban_board using">
+  <h2 class="ui header">
+    <i class="flask icon"></i>
+    <div class="content">
+      USING
+    </div>
+  </h2>
+  <div class="list_area"></div>
+</div>
+`;
+
+export const $DoneList = `
+<div class="kanban_board done">
+  <h2 class="ui header">
+    <i class="flag checkered icon"></i>
+    <div class="content">
+      DONE
+    </div>
+  </h2>
+  <div class="list_area"></div>
+</div>
+`;
+
+export const $ModifyModal = `
+  <div class="modal_contents">
+    <button class="modal_close"><i class="large grey white close icon link"></i></button>
+    <h1 class="modal_header">수정</h1>
+    <div class="modal_body">
+      <form class="ui form">
+        <div class="field">
+          <label>TITLE</label>
+          <input class="title_input" type="text" name="first-name" placeholder="title">
+        </div>
+        <div class="field">
+          <label>Content</label>
+          <input class="content_input" type="text" name="last-name" placeholder="content">
+        </div>
+      </form>
+    </div>
+    <div class="modal_footer">
+      <button class="ui primary button">
+        Save
+      </button>
+    </div>
+  </div>
+`;
