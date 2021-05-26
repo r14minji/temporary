a0e29d30f3e7c1df53599e36ce31427e4a5f58f4
r14minji <rkdalswl0728@gmail.com>
2021-05-26 14:43:41 +0900

    test

--- JS/utils/utils.js
+++ JS/utils/utils.js
@@ -1,11 +1,11 @@
-
-
 export const createDate = () => {
   const year = new Date().getFullYear;
   const month = new Date().getMonth;
   const date = new Date().getDate;
   const time = new Date().getTime;
-  const datetime = `${year}-${month}-${date} ${time}`
-  return datetime
-  console.log(createDate())
-}
+  const datetime = `${year}-${month}-${date} ${time}`;
+  return datetime;
+  console.log(createDate());
+};
+
+//
